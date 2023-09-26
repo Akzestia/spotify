fe06aea0e0f6725c475faeee76c7ba6158f1bfb4
Azure <109163481+AzureXSX@users.noreply.github.com>
2023-09-26 18:40:37 +0200

    Update

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,40 +1,54 @@
-
-
-
 import React from "react";
 import { useNavigate } from "react-router";
-import '../CSS/MainPage.css'
+import "../CSS/MainPage.css";
 
-class MainPage extends React.Component{
+class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props)
+    this.state = {};
+  }
 
-        this.state = {
-
-        }
-    }
-
-    render(){
-        return(
-            <div className="main-div">
-                <div className="content-div">
-
-                </div>
-                <div className="media-player-div">
-                    
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="main-div">
+        <div className="content-div x-hor-div">
+          <div className="x-ver-div">
+            <div className="sn-div-side x-border"></div>
+            <div className="sx-div-side x-border"></div>
+          </div>
+          <div className="sn-div-main x-border">
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+          </div>
+        </div>
+        <div className="media-player-div">
+          <audio controls>
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+      </div>
+    );
+  }
 }
 
+export function MainPageWithRouter(props) {
+  const navigate = useNavigate();
 
-export function MainPageWithRouter(props){
-
-    const navigate = useNavigate();
-
-    return(<MainPage navigate={navigate}></MainPage>)
+  return <MainPage navigate={navigate}></MainPage>;
 }
 
 export default MainPage;
