2fe52d6e6223649230033d7ea30ffd544956a7c9
Azure <109163481+AzureXSX@users.noreply.github.com>
2023-09-26 18:49:10 +0200

    npm i helmet

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router";
+import Helmet from 'react-helmet'
 import "../CSS/MainPage.css";
+import ScriptJs from '../Scripts/mainpage_script'
 
 class MainPage extends React.Component {
   constructor(props) {
@@ -11,36 +13,147 @@ class MainPage extends React.Component {
 
   render() {
     return (
-      <div className="main-div">
-        <div className="content-div x-hor-div">
-          <div className="x-ver-div">
-            <div className="sn-div-side x-border"></div>
-            <div className="sx-div-side x-border"></div>
+      <>
+        <div className="main-div">
+          <div className="content-div x-hor-div">
+            <div className="x-ver-div">
+              <div className="sn-div-side x-border"></div>
+              <div className="sx-div-side x-border"></div>
+            </div>
+            <div className="sn-div-main x-border">
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  height: "30rem",
+                  width: "20rem",
+                  backgroundColor: "aqua",
+                  margin: "2rem",
+                }}
+              ></div>
+            </div>
           </div>
-          <div className="sn-div-main x-border">
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
-            <div style={{height: "30rem", width: "20rem", backgroundColor: "aqua", margin: "2rem"}}></div>
+          <div className="media-player-div">
+            <audio controls>
+              Your browser does not support the audio element.
+            </audio>
           </div>
         </div>
-        <div className="media-player-div">
-          <audio controls>
-            Your browser does not support the audio element.
-          </audio>
-        </div>
-      </div>
+
+        <Helmet>
+           <ScriptJs></ScriptJs>
+        </Helmet>
+      </>
     );
   }
 }
