45ff60637f248acdbf645442f0bcd88c4a8d0528
Azure <109163481+AzureXSX@users.noreply.github.com>
2023-10-01 02:55:10 +0200

    Shuffle && Repeat modes updated

--- src/Components/WepPLayback.js
+++ src/Components/WepPLayback.js
@@ -36,9 +36,13 @@ function WebPlayback(props) {
   const [devixeId, setId] = useState("");
   const navigate = useNavigate();
   const [isnegative, setNegative] = useState(false);
+  const [repeatMode, setMode] = useState("off");
+  const [shuffleState, setShuffle] = useState(false);
 
   var percents;
   var intXint;
+
+
 
   var id_device = "";
 
@@ -60,10 +64,84 @@ function WebPlayback(props) {
 
   function ClickShuffle(){
 
+    setShuffle(!shuffleState)
+    const config = {
+      method: 'put',
+      url: `https://api.spotify.com/v1/me/player/shuffle?state=${shuffleState}`,
+      headers: {
+        Authorization: 'Bearer ' + props.authToken,
+      },
+    };
+    
+    axios(config)
+      .then((response) => {
+        // Handle success response
+      })
+      .catch((error) => {
+        console.log(error)
+      });
   }
 
-  function ClickReplay(){
-    
+  function ClickRepeat(){
+    switch(repeatMode){
+      case 'track':{
+        setMode('context')
+        const config = {
+          method: 'put',
+          url: `https://api.spotify.com/v1/me/player/repeat?state=${repeatMode}`,
+          headers: {
+            Authorization: 'Bearer ' + props.authToken,
+          },
+        };
+        
+        axios(config)
+        .then((response) => {
+          console.log(repeatMode)
+        })
+        .catch((error) => {
+          console.log(error)
+        });
+      }
+      break;
+      case 'context':{
+        setMode('off')
+        const config = {
+          method: 'put',
+          url: `https://api.spotify.com/v1/me/player/repeat?state=${repeatMode}`,
+          headers: {
+            Authorization: 'Bearer ' + props.authToken,
+          },
+        };
+        
+        axios(config)
+        .then((response) => {
+          console.log(repeatMode)
+        })
+        .catch((error) => {
+          console.log(error)
+        });
+      }
+      break;
+      case 'off':{
+        setMode('track')
+        const config = {
+          method: 'put',
+          url: `https://api.spotify.com/v1/me/player/repeat?state=${repeatMode}`,
+          headers: {
+            Authorization: 'Bearer ' + props.authToken,
+          },
+        };
+        
+        axios(config)
+          .then((response) => {
+            console.log(repeatMode)
+          })
+          .catch((error) => {
+            console.log(error)
+          });
+      }
+      break;
+    }
   }
 
   useEffect(() => {
@@ -488,7 +566,7 @@ function WebPlayback(props) {
               <path d="M12.7 1a.7.7 0 0 0-.7.7v5.15L2.05 1.107A.7.7 0 0 0 1 1.712v12.575a.7.7 0 0 0 1.05.607L12 9.149V14.3a.7.7 0 0 0 .7.7h1.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-1.6z"></path>
             </svg>
           </button>
-          <button className="xxx-next-prev" onClick={ClickReplay}>
+          <button className="xxx-next-prev" onClick={ClickRepeat}>
             <svg
               role="img"
               height="16"
