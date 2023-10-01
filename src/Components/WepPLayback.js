5ed17ffd7d1823c136f6a521261cd9fe73ab5359
Azure <109163481+AzureXSX@users.noreply.github.com>
2023-10-01 02:43:21 +0200

    UI update

--- src/Components/WepPLayback.js
+++ src/Components/WepPLayback.js
@@ -58,6 +58,14 @@ function WebPlayback(props) {
     player.togglePlay();
   }
 
+  function ClickShuffle(){
+
+  }
+
+  function ClickReplay(){
+    
+  }
+
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://sdk.scdn.co/spotify-player.js";
@@ -95,8 +103,7 @@ function WebPlayback(props) {
         };
 
         axios(options)
-          .then((response) => {
-          })
+          .then((response) => {})
           .catch((error) => {
             console.log(error);
           });
@@ -154,36 +161,33 @@ function WebPlayback(props) {
   }, []);
 
   if (!is_active) {
-
     if (devixeId != "") {
-
-
       const configx = {
-        method: 'get',
-        url: 'https://api.spotify.com/v1/me/player/recently-played?limit=1',
+        method: "get",
+        url: "https://api.spotify.com/v1/me/player/recently-played?limit=1",
         headers: {
-          Authorization: 'Bearer ' + props.authToken,
+          Authorization: "Bearer " + props.authToken,
         },
       };
-      
+
       axios(configx)
         .then((response) => {
           const config = {
-            method: 'put',
+            method: "put",
             url: `https://api.spotify.com/v1/me/player/play?device_id=${devixeId}`,
             headers: {
-              Authorization: 'Bearer ' + props.authToken,
-              'Content-Type': 'application/json',
+              Authorization: "Bearer " + props.authToken,
+              "Content-Type": "application/json",
             },
             data: {
               uris: [response.data.items[0].track.uri],
               position_ms: 0,
             },
           };
-          
+
           axios(config)
             .then((response) => {
-              setActive(true)
+              setActive(true);
             })
             .catch((error) => {
               // Handle error response
@@ -192,7 +196,6 @@ function WebPlayback(props) {
         .catch((error) => {
           // Handle error response
         });
-
     }
     return (
       <div
@@ -367,35 +370,32 @@ function WebPlayback(props) {
     );
   } else if (player) {
     if (devixeId != "" && !is_active) {
-
-
       const configx = {
-        method: 'get',
-        url: 'https://api.spotify.com/v1/me/player/recently-played?limit=1',
+        method: "get",
+        url: "https://api.spotify.com/v1/me/player/recently-played?limit=1",
         headers: {
-          Authorization: 'Bearer ' + props.authToken,
+          Authorization: "Bearer " + props.authToken,
         },
       };
-      
+
       axios(configx)
         .then((response) => {
-
           const config = {
-            method: 'put',
+            method: "put",
             url: `https://api.spotify.com/v1/me/player/play?device_id=${devixeId}`,
             headers: {
-              Authorization: 'Bearer ' + props.authToken,
-              'Content-Type': 'application/json',
+              Authorization: "Bearer " + props.authToken,
+              "Content-Type": "application/json",
             },
             data: {
               uris: [response.data.items[0].track.uri],
               position_ms: 0,
             },
           };
-          
+
           axios(config)
             .then((response) => {
-              setActive(true)
+              setActive(true);
             })
             .catch((error) => {
               // Handle error response
@@ -404,8 +404,6 @@ function WebPlayback(props) {
         .catch((error) => {
           // Handle error response
         });
-
-    
     }
 
     return (
@@ -414,6 +412,20 @@ function WebPlayback(props) {
         style={{ display: "flex", flexDirection: "column" }}
       >
         <div className="div-x-azure">
+          <button className="xxx-next-prev" onClick={ClickShuffle}>
+            <svg
+              role="img"
+              height="16"
+              width="16"
+              aria-hidden="true"
+              viewBox="0 0 16 16"
+              data-encore-id="icon"
+              class="Svg-sc-ytk21e-0 haNxPq"
+            >
+              <path d="M13.151.922a.75.75 0 1 0-1.06 1.06L13.109 3H11.16a3.75 3.75 0 0 0-2.873 1.34l-6.173 7.356A2.25 2.25 0 0 1 .39 12.5H0V14h.391a3.75 3.75 0 0 0 2.873-1.34l6.173-7.356a2.25 2.25 0 0 1 1.724-.804h1.947l-1.017 1.018a.75.75 0 0 0 1.06 1.06L15.98 3.75 13.15.922zM.391 3.5H0V2h.391c1.109 0 2.16.49 2.873 1.34L4.89 5.277l-.979 1.167-1.796-2.14A2.25 2.25 0 0 0 .39 3.5z"></path>
+              <path d="m7.5 10.723.98-1.167.957 1.14a2.25 2.25 0 0 0 1.724.804h1.947l-1.017-1.018a.75.75 0 1 1 1.06-1.06l2.829 2.828-2.829 2.828a.75.75 0 1 1-1.06-1.06L13.109 13H11.16a3.75 3.75 0 0 1-2.873-1.34l-.787-.938z"></path>
+            </svg>
+          </button>
           <button className="xxx-next-prev" onClick={ClickPREVIOUS}>
             <svg
               role="img"
@@ -474,6 +486,19 @@ function WebPlayback(props) {
               class="Svg-sc-ytk21e-0 haNxPq"
             >
               <path d="M12.7 1a.7.7 0 0 0-.7.7v5.15L2.05 1.107A.7.7 0 0 0 1 1.712v12.575a.7.7 0 0 0 1.05.607L12 9.149V14.3a.7.7 0 0 0 .7.7h1.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-1.6z"></path>
+            </svg>
+          </button>
+          <button className="xxx-next-prev" onClick={ClickReplay}>
+            <svg
+              role="img"
+              height="16"
+              width="16"
+              aria-hidden="true"
+              viewBox="0 0 16 16"
+              data-encore-id="icon"
+              class="Svg-sc-ytk21e-0 haNxPq"
+            >
+              <path d="M0 4.75A3.75 3.75 0 0 1 3.75 1h8.5A3.75 3.75 0 0 1 16 4.75v5a3.75 3.75 0 0 1-3.75 3.75H9.81l1.018 1.018a.75.75 0 1 1-1.06 1.06L6.939 12.75l2.829-2.828a.75.75 0 1 1 1.06 1.06L9.811 12h2.439a2.25 2.25 0 0 0 2.25-2.25v-5a2.25 2.25 0 0 0-2.25-2.25h-8.5A2.25 2.25 0 0 0 1.5 4.75v5A2.25 2.25 0 0 0 3.75 12H5v1.5H3.75A3.75 3.75 0 0 1 0 9.75v-5z"></path>
             </svg>
           </button>
           <input
