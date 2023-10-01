7a488d729e452bcc55eee25f6d2801902e58c1e2
Azure <109163481+AzureXSX@users.noreply.github.com>
2023-10-01 03:16:32 +0200

    Update

--- src/Components/WepPLayback.js
+++ src/Components/WepPLayback.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../CSS/App.css";
-import apiCLient from "./ApiClient";
+import "../CSS/WebPlayback.css";
 import "../CSS/MainPage.css";
 import { useLocation, useNavigate } from "react-router";
 
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,16 +1,11 @@
 import React, { useCallback } from "react";
 import { useLocation, useNavigate } from "react-router";
-import Helmet from "react-helmet";
 import "../CSS/MainPage.css";
-import TrackCard from "../Components/TrackCardComponent";
 import axios from "axios";
-import { useEffect, useState } from "react";
 import List from "../Components/TracksListComponent";
 import "../Scripts/mainpage_script";
 import "media-chrome";
-import { debounce } from "lodash";
 import WebPlayback from "../Components/WepPLayback";
-import { Buffer } from "buffer";
 import LikedTracksList from "../Components/LikedTracksComponent";
 
 class MainPage extends React.Component {
