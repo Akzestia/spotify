cf8b890876da95924c9e70148cd60b49440d3300
Azure <109163481+AzureXSX@users.noreply.github.com>
2023-10-01 03:13:13 +0200

    update

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
 
 
 
-const express = require('express')
-const dotenv = require('dotenv');
+// const express = require('express')
+// const dotenv = require('dotenv');
 
-const port = 5000
+// const port = 5000
 
-dotenv.config()
+// dotenv.config()
 
-var spotify_client_id = process.env.SPOTIFY_CLIENT_ID
-var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
+// var spotify_client_id = process.env.SPOTIFY_CLIENT_ID
+// var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
 
-var app = express();
+// var app = express();
 
 
-app.get('/auth/login', (req, res) => {
-    console.log(spotify_client_id)
-    console.log(spotify_client_secret)
+// app.get('/auth/login', (req, res) => {
+//     console.log(spotify_client_id)
+//     console.log(spotify_client_secret)
     
-});
+// });
 
-app.get('/auth/callback', (req, res) => {
-});
+// app.get('/auth/callback', (req, res) => {
+// });
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Listening at http://localhost:${port}`)
+// })
