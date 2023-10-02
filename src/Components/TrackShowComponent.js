b4afc0d68cba22630c8ecb72f6ad6d5a4e31c3b8
アズールシャドウ <109163481+AzureXSX@users.noreply.github.com>
2023-10-02 15:19:56 +0200

    Update

--- src/Components/TrackShowComponent.js
+++ src/Components/TrackShowComponent.js
@@ -8,25 +8,41 @@ import "media-chrome";
 import WebPlayback from "../Components/WepPLayback";
 import LikedTracksList from "../Components/LikedTracksComponent";
 import PlayList_List from "../Components/PlaylistsLIST_Component";
-import PlayList, {PlaylistWithRouter} from "../Components/PlaylistComponent";
+import PlayList, { PlaylistWithRouter } from "../Components/PlaylistComponent";
 
 
 
+class TrackShowComponent extends React.Component {
+    constructor(props) {
+        super(props);
 
-
-export function TracShowComponentWithRouter(props) {
-    const navigate = useNavigate();
-  
-    const location = useLocation();
-  
-    if (location.state.authToken != null) {
-      return (
-        <MainPage navigate={navigate} token={location.state.authToken}></MainPage>
-      );
-    } else {
-      console.log("TOKEN BAD");
-      navigate("/");
+        this.state = {
+            authToken: this.props.token,
+            trackId: this.props.id,
+            trackUri: this.props.trackUri,
+        }
     }
-  }
-  
-  export default MainPage;
+}
+
+
+
+export function TrackShowComponentWithRouter(props) {
+    const navigate = useNavigate();
+
+    const location = useLocation();
+
+    if (location.state.authToken != null) {
+        return (
+            <MainPage trackId={location.state.track.Id} 
+            trackUri={location.state.track.uri}
+            navigate={navigate} token={location.state.authToken}></MainPage>
+        );
+    } else {
+        console.log("TOKEN BAD");
+        navigate("/");
+    }
+
+
+}
+
+export default MainPage;
